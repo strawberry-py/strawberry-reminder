cee99cb2157ce705a3d4a6cf0a73e35b52a5d27b
418m <91502152+418m@users.noreply.github.com>
2022-10-28 21:42:59 +0200

    Countdown module

---
+++ countdown/database.py
@@ -0,0 +1,170 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import BigInteger, Column, Integer, String, DateTime
+from typing import List, Dict, Optional
+
+from pie.database import database, session
+
+
+class CountdownItem(database.base):
+    """Represents a database Countdown item for :class:`Reminder` module.
+
+    Attributes:
+        idx: The database ID.
+        guild_id: ID of the guild.
+        author_id: User ID of reminder author.
+        permalink: Message URL.
+        message: Reminder text (None if empty).
+        origin_date: Date of creation.
+        countdown_date: Date for reminding
+        status: Status of reminder
+    """
+
+    __tablename__ = "fun_countdown_item"
+
+    idx = Column(Integer, primary_key=True, autoincrement=True)
+    guild_id = Column(BigInteger, default=None)
+    author_id = Column(BigInteger, default=None)
+    name = Column(String, default=None)
+    permalink = Column(String, default=None)
+    message = Column(String, default=None)
+    origin_date = Column(DateTime(timezone=True), default=None)
+    countdown_date = Column(DateTime(timezone=True), default=None)
+
+    @staticmethod
+    def add(
+        guild_id: int,
+        author_id: int,
+        name: str,
+        permalink: str,
+        message: str,
+        origin_date: datetime,
+        countdown_date: datetime,
+    ) -> CountdownItem:
+        """Creates a new CountdownItem in the database.
+
+        Args:
+            author_id: Countdown author.
+            name: Event name
+            permalink: URL of countdown message.
+            message: Countdown text (None if empty).
+            origin_date: Date of creation.
+            countdown_date: Date for reminding
+
+        Raises:
+            ValueError: End time already passed.
+
+        Returns:
+            The created database item.
+        """
+
+        origin_date = datetime.now()
+        if countdown_date < origin_date:
+            raise ValueError
+
+        item = CountdownItem(
+            guild_id=guild_id,
+            author_id=author_id,
+            name=name,
+            permalink=permalink,
+            message=message,
+            origin_date=origin_date,
+            countdown_date=countdown_date,
+        )
+
+        session.add(item)
+        session.commit()
+        return item
+
+    @staticmethod
+    def get(author_id: int, name: str) -> Optional[CountdownItem]:
+        query = (
+            session.query(CountdownItem)
+            .filter_by(author_id=author_id, name=name)
+            .one_or_none()
+        )
+        return query
+
+    @staticmethod
+    def get_all(
+        guild_id: int = None,
+        author_id: int = None,
+        min_origin_date: datetime = None,
+        max_origin_date: datetime = None,
+        min_countdown_date: datetime = None,
+        max_countdown_date: datetime = None,
+    ) -> List[CountdownItem]:
+        """Retreives List of CountdownItem filtered by Guild ID.
+
+        Args:
+            guild_id: Guild whose items are to be returned.
+            author_id: User whose items are to be returned.
+            min_origin_date: Filter items created after this date.
+            max_origin_date: Filter items created before this date.
+            min_countdown_date: Filter items being reminded after this date.
+            max_countdown_date: Filter items being reminded before this date.
+
+        Returns:
+            :class:`List[CountdownItem]`: The retrieved countdown items ordered by countdown_date descending.
+        """
+        query = session.query(CountdownItem)
+
+        if guild_id is not None:
+            query = query.filter_by(guild_id=guild_id)
+
+        if author_id is not None:
+            query = query.filter_by(author_id=author_id)
+
+        if min_origin_date is not None:
+            query = query.filter(CountdownItem.origin_date > min_origin_date)
+
+        if max_origin_date is not None:
+            query = query.filter(CountdownItem.origin_date < max_origin_date)
+
+        if min_countdown_date is not None:
+            query = query.filter(CountdownItem.countdown_date > min_countdown_date)
+
+        if max_countdown_date is not None:
+            query = query.filter(CountdownItem.countdown_date < max_countdown_date)
+
+        query = query.order_by(CountdownItem.countdown_date.desc())
+
+        return query.all()
+
+    def delete(self):
+        """
+        Deletes the item from the database.
+        """
+        session.delete(self)
+        session.commit()
+
+    def __repr__(self) -> str:
+        return (
+            f'<ReminderItem idx="{self.idx}" guild_id="{self.guild_id}" '
+            f'author_id="{self.author_id}" name="{self.name}">'
+            f'permalink="{self.permalink}" message="{self.message}" '
+            f'origin_date="{self.origin_date}" remind_date="{self.countdown_date}">'
+        )
+
+    def dump(self) -> Dict:
+        """Dumps CountdownItem into a dictionary.
+
+        Returns:
+            :class:`Dict`: The CountdownItem as a dictionary.
+        """
+        return {
+            "idx": self.idx,
+            "guild_id": self.guild_id,
+            "author_id": self.author_id,
+            "name": self.name,
+            "permalink": self.permalink,
+            "message": self.message,
+            "origin_date": self.origin_date,
+            "countdown_date": self.countdown_date,
+        }
+
+    def save(self):
+        """Commits the CountdownItem to the database."""
+        session.commit()
---
+++ countdown/module.py
@@ -0,0 +1,242 @@
+from datetime import datetime
+from typing import Optional, List
+
+import dateutil.parser
+import dateutil.relativedelta
+from discord.ext import commands
+
+from pie.utils import ConfirmView
+from .database import CountdownItem
+from pie import check, i18n, logger, utils
+
+_ = i18n.Translator("modules/fun").translate
+bot_log = logger.Bot.logger()
+guild_log = logger.Guild.logger()
+
+
+class Countdown(commands.Cog):
+    """Countdown"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def _process_text(self, ctx: commands.Context, text: Optional[str]):
+        """Shortens text to 1024 characters. Places end of the code block at the end if the truncated text contains unclosed block."""
+        if text is not None and len(text) > 1024:
+            text = text[:1024]
+            text = text[:-3] + "```" if text.count("```") % 2 != 0 else text
+
+        return text
+
+    def _get_remaining_time(self, countdown_item: CountdownItem) -> str:
+        if countdown_item.countdown_date < datetime.now():
+            return "Finished"
+        else:
+            return utils.time.format_seconds(
+                (countdown_item.countdown_date - datetime.now()).total_seconds()
+            )
+
+    async def _get_embed(self, ctx, countdown: CountdownItem):
+        embed = utils.discord.create_embed(
+            author=ctx.author,
+            title=_(ctx, "Countdown {name}").format(name=countdown.name),
+            description=countdown.message,
+        )
+        embed.add_field(
+            name=_(ctx, "Date"),
+            value=utils.time.format_datetime(countdown.countdown_date),
+        )
+        embed.add_field(
+            name=_(ctx, "Remaining time"), value=self._get_remaining_time(countdown)
+        )
+        embed.add_field(
+            name=_(ctx, "Created on"),
+            value=utils.time.format_datetime(countdown.origin_date),
+            inline=False,
+        )
+        embed.add_field(name=_(ctx, "URL"), value=countdown.permalink)
+
+        return embed
+
+    async def _send_countdown_list(self, ctx, query):
+        countdowns = []
+
+        for item in query:
+
+            countdown = CountdownDummy()
+            countdown.idx = item.idx
+            countdown.name = item.name
+            countdown.countdown_date = item.countdown_date.strftime("%Y-%m-%d %H:%M")
+            countdown.remaining_time = self._get_remaining_time(item)
+            countdown.message = item.message
+            if len(countdown.message) > 30:
+                countdown.message = countdown.message[:29] + "\N{HORIZONTAL ELLIPSIS}"
+
+            countdowns.append(countdown)
+
+        table_columns: dict = {
+            "idx": _(ctx, "ID"),
+            "name": _(ctx, "Name"),
+            "countdown_date": _(ctx, "Countdown date"),
+            "remaining_time": _(ctx, "Remaining time"),
+            "message": _(ctx, "Message"),
+        }
+
+        table_pages: List[str] = utils.text.create_table(
+            countdowns[::-1], table_columns
+        )
+
+        for table_page in table_pages:
+            await ctx.send("```" + table_page.replace("`", "'") + "```")
+
+    @check.acl2(check.ACLevel.MEMBER)
+    @commands.group(name="countdown")
+    async def countdown_(self, ctx: commands.Context):
+        await utils.discord.send_help(ctx)
+
+    @check.acl2(check.ACLevel.MEMBER)
+    @countdown_.command(name="set")
+    async def countdown_set(
+        self, ctx: commands.Context, name: str, datetime_str: str, text: Optional[str]
+    ):
+        """Set new countdown.
+
+        Args:
+            name: Countdown name
+            datetime_str: Datetime string (preferably quoted)
+            text: Optional message
+        """
+
+        if CountdownItem.get(ctx.author.id, name):
+            await ctx.reply(
+                _(ctx, "Countdown '{name}' already exists.").format(name=name)
+            )
+            return
+
+        text = await self._process_text(ctx, text)
+
+        try:
+            date = utils.time.parse_datetime(datetime_str)
+        except dateutil.parser.ParserError:
+            await ctx.reply(
+                _(
+                    ctx,
+                    "I don't know how to parse `{datetime_str}`, pleasy try again.",
+                ).format(datetime_str=datetime_str)
+            )
+            return
+
+        if date < datetime.now():
+            await ctx.reply(_(ctx, "Time must be in future."))
+            return
+
+        item = CountdownItem.add(
+            guild_id=ctx.author.guild.id if hasattr(ctx.author, "guild") else 0,
+            author_id=ctx.author.id,
+            name=name,
+            permalink=ctx.message.jump_url,
+            message=text,
+            origin_date=ctx.message.created_at,
+            countdown_date=date,
+        )
+
+        await bot_log.debug(
+            ctx.author,
+            ctx.channel,
+            f"Countdown #{item.idx} ({item.countdown_date}) "
+            f"created for {ctx.author.name}.",
+        )
+        await ctx.message.add_reaction("✅")
+
+        await ctx.reply(
+            _(
+                ctx,
+                "Countdown '{name}' to **{date}** created. Remaining time is {time}.",
+            ).format(
+                name=name,
+                date=utils.time.format_datetime(item.countdown_date),
+                time=self._get_remaining_time(item),
+            )
+        )
+
+    @check.acl2(check.ACLevel.MEMBER)
+    @countdown_.command(name="get")
+    async def countdown_get(self, ctx: commands.Context, name: str):
+        """Get countdown details.
+
+        Args:
+            name: Countdown name
+        """
+        countdown = CountdownItem.get(author_id=ctx.author.id, name=name)
+
+        if not countdown:
+            await ctx.reply(
+                _(ctx, "Countdown '{name}' does not exist.").format(name=name)
+            )
+            return
+
+        if ctx.author.id != countdown.author_id:
+            await ctx.send(_(ctx, "You don't have permission to see this countdown."))
+            return
+
+        embed = await self._get_embed(ctx, countdown=countdown)
+
+        await ctx.reply(embed=embed)
+
+    @check.acl2(check.ACLevel.MEMBER)
+    @countdown_.command(name="delete")
+    async def countdown_delete(self, ctx: commands.Context, name: str):
+        """Delete countdown
+
+        Args:
+            name: Countdown name
+        """
+        countdown = CountdownItem.get(author_id=ctx.author.id, name=name)
+        if not countdown:
+            await ctx.reply(
+                _(ctx, "Countdown '{name}' does not exist.").format(name=name)
+            )
+            return
+
+        embed = await self._get_embed(ctx, countdown=countdown)
+        embed.title = _(ctx, "Do you want to delete this countdown?")
+        view = ConfirmView(ctx, embed)
+
+        value = await view.send()
+        if value is None:
+            await ctx.send(_(ctx, "Deleting timed out."))
+        elif value:
+            countdown.delete()
+            await ctx.send(_(ctx, "Countdown deleted."))
+            await bot_log.debug(
+                ctx.author,
+                ctx.channel,
+                f"Countdown #{countdown.idx} ({countdown.countdown_date}) deleted.",
+            )
+        else:
+            await ctx.send(_(ctx, "Deleting aborted."))
+
+    @check.acl2(check.ACLevel.MEMBER)
+    @countdown_.command(name="list")
+    async def countdown_list(self, ctx: commands.Context, show_finished: bool = False):
+        """List your countdowns
+
+        Args:
+            show_finished: True/False whether to show finished countdowns
+        """
+        if show_finished:
+            query = CountdownItem.get_all(author_id=ctx.author.id)
+        else:
+            query = CountdownItem.get_all(
+                author_id=ctx.author.id, min_countdown_date=datetime.now()
+            )
+
+        await self._send_countdown_list(ctx, query)
+
+
+class CountdownDummy:
+    pass
+
+
+async def setup(bot) -> None:
+    await bot.add_cog(Countdown(bot))
