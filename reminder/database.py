557039a3148fe3be788649ce6082568b484d52cb
ArcasCZ <VydraMan@seznam.cz>
2024-04-13 13:03:29 +0200

    [Isort] Move Discord.py to own section

--- reminder/database.py
+++ reminder/database.py
@@ -4,8 +4,9 @@ import enum
 from datetime import datetime
 from typing import Dict, List
 
-import discord
 from sqlalchemy import BigInteger, Column, DateTime, Enum, Integer, String
+
+import discord
 
 from pie.database import database, session
 
